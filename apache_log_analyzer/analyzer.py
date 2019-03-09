d0e8a46fc7a2f3d60350ab896e5d20d4e26f7412
yuuki <yuuki0xff@gmail.com>
2019-03-09 14:42:12 +0900

    add type hints to Parser.parse()

--- apache_log_analyzer/analyzer.py
+++ apache_log_analyzer/analyzer.py
@@ -1,7 +1,11 @@
 from typing import (
+    NewType,
     Optional,
 )
+
 import apache_log_parser
+
+LogRecord = NewType('LogRecord', dict)
 
 DEFAULT_FORMAT = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
 
@@ -11,3 +15,6 @@ class Parser(apache_log_parser.Parser):
         if format_string is None:
             format_string = DEFAULT_FORMAT
         super().__init__(format_string)
+
+    def parse(self, log_line: str) -> LogRecord:
+        return super().parse(log_line)
